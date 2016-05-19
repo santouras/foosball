4e25ce798e757d6b9baeca869f940c75398e61a3
Adam Bolte <abolte@systemsaviour.com>
2016-05-20 04:30:02 +1000

    Rename User display_name column to username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422051511) do
+ActiveRecord::Schema.define(version: 20160519182634) do
 
   create_table "games", force: :cascade do |t|
     t.integer  "score_user1"
@@ -28,10 +28,10 @@ ActiveRecord::Schema.define(version: 20160422051511) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "display_name"
+    t.string   "username"
     t.float    "points"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
