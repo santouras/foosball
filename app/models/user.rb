581c58fa7f44e8bd2752bf57d8dac07a63b8f5d6
Adam Bolte <abolte@systemsaviour.com>
2016-05-20 15:31:48 +1000

    Add User model nickname/naming updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ActiveRecord::Base
   def games
     Game.where('user1_id = :id OR user2_id = :id', id: id)
   end
+
+  def display_name
+    nickname || full_name
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
